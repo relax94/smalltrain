95707b50bd0f5f157211cbbf0c3f4bd437c515df
relax94 <basket.pavlenko@gmail.com>
2017-02-20 10:47:19 +0200

    add json backend formatters

---
+++ _services/traveler/src/traveler/Controllers/ApiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using traveler.Interfaces;
+using traveler.Models;
+
+namespace traveler.Controllers
+{
+    public class ApiController : Controller
+    {
+        private IDozorService _dozorService;
+
+        public ApiController(IDozorService dozorService)
+        {
+            _dozorService = dozorService;
+        }
+
+        [HttpGet("/api/dozor/routes")]
+        public async Task<IActionResult> MakeDozorRequesTask(int routeId)
+        {
+            return Ok(await this._dozorService.ScrapDozorApiByRouteId(routeId));
+        }
+        
+    }
+}
