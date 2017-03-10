57d3ba4951675a488a7e67ec45c07388765b79a0
relax94 <basket.pavlenko@gmail.com>
2017-03-10 13:22:22 +0200

    add acco ui

--- src/components/DozorObservablePoint.js
+++ src/components/DozorObservablePoint.js
@@ -4,12 +4,12 @@ export class DozorObservablePoint extends Component {
 
 
     applyAddingObservablePoint() {
-            const {updateObservablePoint} = this.props.routeActions;
-            updateObservablePoint(this.props.index, {
-                ...this.props.item,
-                label: this.state.label,
-                duration: this.state.duration
-            });
+        const {updateObservablePoint} = this.props.routeActions;
+        updateObservablePoint(this.props.index, {
+            ...this.props.item,
+            label: this.state.label,
+            duration: this.state.duration
+        });
     }
 
     handlePointDetailsChanges(e) {
@@ -26,29 +26,41 @@ export class DozorObservablePoint extends Component {
     render() {
         const {item, index, isAlert} = this.props;
         return (
-            <li key={index}
-                className={'list-group-item justify-content-between ' + (isAlert ? 'active' : '')}
-            >
-                Point# {item.lat}
-                <div className="row">
-                    <div className="col-md-4">
-                        <input type="text" name="label" className="form-control"
-                               onChange={::this.handlePointDetailsChanges}/>
-                    </div>
-                    <div className="col-md-4">
-                        <input type="text" name="duration" className="form-control"
-                               onChange={::this.handlePointDetailsChanges}/>
-                    </div>
-                    <div className="col-md-2">
-                        <button className="btn btn-primary" onClick={::this.applyAddingObservablePoint}>OK
-                        </button>
-                    </div>
-                    <div className="col-md-2">
-                        <button className="btn btn-danger" onClick={::this.removePointBtnClick}>Remove
-                        </button>
+
+            <div key={index} className={'card ' + (isAlert ? 'active' : '')}>
+                <div className="card-header" role="tab" id={'heading' + index}>
+                    <h5 className="mb-0">
+                        <a className="collapsed" data-toggle="collapse" data-parent="#accordion"
+                           href={'#collapse' + index}
+                           aria-expanded="false" aria-controls="collapseThree">
+                            Point# {item.label || item.lat}
+                        </a>
+                    </h5>
+                </div>
+                <div id={'collapse' + index} className="collapse" role="tabpanel" aria-labelledby={'heading' + index}>
+                    <div className="card-block">
+                        <div className="row">
+                            <div className="col-md-4">
+                                <input placeholder="Point Label" type="text" name="label" className="form-control"
+                                       onChange={::this.handlePointDetailsChanges}/>
+                            </div>
+                            <div className="col-md-4">
+                                <input placeholder="Vibration Duration" type="text" name="duration"
+                                       className="form-control"
+                                       onChange={::this.handlePointDetailsChanges}/>
+                            </div>
+                            <div className="col-md-2">
+                                <button className="btn btn-primary" onClick={::this.applyAddingObservablePoint}>OK
+                                </button>
+                            </div>
+                            <div className="col-md-2">
+                                <button className="btn btn-danger" onClick={::this.removePointBtnClick}>Remove
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </li>
+            </div>
         )
     }
 
